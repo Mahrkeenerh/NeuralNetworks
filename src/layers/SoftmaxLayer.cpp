d9dd1d4cb41ac2cf27ee47ff3da06f9b2330a5a2
Lucie Novotna <lnovotna@redhat.com>
2022-10-09 11:57:22 +0200

    Cross-entropy loss for softmax layer

--- src/layers/SoftmaxLayer.cpp
+++ src/layers/SoftmaxLayer.cpp
@@ -16,6 +16,13 @@ SoftmaxLayer::SoftmaxLayer(int input_size, int output_size) {
         std::vector<std::vector<double>>(output_size, std::vector<double>(input_size + 1, 1.0));
     this->errors = std::vector<double>(output_size, 0.0);
     this->outputs = std::vector<double>(output_size, 0.0);
+
+    // Initialize weights
+    for (int i = 0; i < output_size; i++) {
+        for (int j = 0; j < input_size + 1; j++) {
+            this->weights[i][j] = randn() * sqrt(2.0 / input_size);
+        }
+    }
 }
 
 std::vector<double> SoftmaxLayer::predict(std::vector<double> input) {
@@ -23,31 +30,33 @@ std::vector<double> SoftmaxLayer::predict(std::vector<double> input) {
     // Calculate output for each neuron
     double sum = 0;
     for (int n_i = 0; n_i < this->output_size; n_i++) {
-        this->outputs[n_i] = input[n_i];
+        this->outputs[n_i] = this->weights[n_i][0];
+
+        for (int i = 0; i < this->input_size; i++) {
+            this->outputs[n_i] += this->weights[n_i][i + 1] * input[i];
+        }
         sum += exp(this->outputs[n_i]);
     }
 
     for (int n_i = 0; n_i < this->output_size; n_i++) {
-        this->outputs[n_i] = exp(this->outputs[n_i] - *std::max_element(std::begin(this->outputs), std::end(this->outputs))) / sum;
+        this->outputs[n_i] = exp(this->outputs[n_i] - 0.0001) / sum;
     }
 
     return this->outputs;
 }
 
 void SoftmaxLayer::out_errors(std::vector<double> target_vector) {
-    // Calculate errors - MSE
-     for (int n_i = 0; n_i < this->output_size; n_i++) {
-         this->errors[n_i] = (this->outputs[n_i] - target_vector[n_i]);
-     }
+    // Calculate errors - MSE and apply activation function
+    // for (int n_i = 0; n_i < this->output_size; n_i++) {
+    //    this->errors[n_i] = (this->outputs[n_i] - target_vector[n_i]) * this->derivative(this->outputs[n_i]);
+    // }
 
-    // Calculate errors - Cross entropy
-    //for (int n_i = 0; n_i < this->output_size; n_i++) {
-    //    this->errors[n_i] = - target_vector[n_i] * log(this->outputs[n_i]);
-    //}
-
-    // Apply activation function
+    // Derivative of cross entropy loss
     for (int n_i = 0; n_i < this->output_size; n_i++) {
-        this->errors[n_i] *= this->derivative(this->outputs[n_i]);
+        this->errors[n_i] = this->outputs[n_i];
+        if (target_vector[n_i] == 1) {
+            this->errors[n_i] -= 1;
+        }
     }
 }
 
@@ -58,5 +67,10 @@ void SoftmaxLayer::backpropagate(Layer* connected_layer, std::vector<double> tar
 
 void SoftmaxLayer::update_weights(std::vector<double> input, double learning_rate) {
     // #pragma omp parallel for
-    return;
+    for (int n_i = 0; n_i < this->output_size; n_i++) {
+        for (int w_i = 1; w_i < this->input_size + 1; w_i++) {
+            this->weights[n_i][w_i] -= this->errors[n_i] * learning_rate * input[w_i - 1];
+        }
+        this->weights[n_i][0] -= this->errors[n_i] * learning_rate;
+    //}
 }
