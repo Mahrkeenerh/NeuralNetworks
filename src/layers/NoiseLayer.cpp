49848d71d74243ce77fcb446cc6e6970673c1903
Samuel Bubán <xbuban1@fi.muni.cz>
2022-10-15 23:51:14 +0200

    Fix noise layer

--- src/layers/NoiseLayer.cpp
+++ src/layers/NoiseLayer.cpp
@@ -16,10 +16,9 @@ NoiseLayer::NoiseLayer(int output_size, double noise_chance, double noise_scale)
 std::vector<double> NoiseLayer::forwardpropagate(std::vector<double> input) {
     // Calculate output for each neuron
     for (int n_i = 0; n_i < this->output_size; n_i++) {
+        this->outputs[n_i] = input[n_i];
         if (rand() / (double)RAND_MAX > this->noise_chance) {
-            this->outputs[n_i] = input[n_i] + (randn() * this->noise_scale);
-        } else {
-            this->outputs[n_i] = 0.0;
+            this->outputs[n_i] += (randn() * this->noise_scale);
         }
     }
 
