521acef3c6ee02a2d66f27b31e12041471de70d8
Samuel Bubán <xbuban1@fi.muni.cz>
2022-10-19 11:42:52 +0200

    Remove .exe extension

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-"g++.exe" -fdiagnostics-color=always -g $(find . -type f -iregex '.*\.[c|h]pp') -o 'main.exe' -fopenmp -Ofast
+"g++.exe" -fdiagnostics-color=always -g $(find . -type f -iregex '.*\.[c|h]pp') -o 'main' -fopenmp -Ofast
