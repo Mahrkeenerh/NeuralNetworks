448773bfa21826f6093a351aeb21e279a483e79b
Samuel Bubán <xbuban1@fi.muni.cz>
2022-11-04 11:57:01 +0100

    Add compilation warnings

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 time_start=$(date +%s%3N)
 
-"g++" -fdiagnostics-color=always -g $(find . -type f -iregex '.*\.[c|h]pp') -fopenmp -Ofast -o main
+"g++" -fdiagnostics-color=always -g $(find . -type f -iregex '.*\.[c|h]pp') -Wall -fopenmp -Ofast -o main
 
 time_end=$(date +%s%3N)
 time_diff=$((time_end - time_start))
